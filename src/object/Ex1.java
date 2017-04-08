f3c29700de9c28e84b2dcff4575e149e47544db6
Yuelin Yan <yuelinyan@gmail.com>
2017-04-07 20:41:01 -0600

    first commit

---
+++ src/object/Ex1.java
@@ -0,0 +1,13 @@
+package object;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		char a;
+//		System.out.println(i);
+//		System.out.println(a);
+	}
+
+}
